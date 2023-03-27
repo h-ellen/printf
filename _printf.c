6b3abff6e874610be55c6931834980318a507f9a
Hellen Arinaitwe <harinaitwe336@gmail.com>
2023-03-27 10:11:21 +0300

    printf.c

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	int count = o;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+				{
+					char c = (char) va_arg(args, int);
+					putchar(c);
+					count++;
+					break;
+				}
+				case 's':
+				{
+					char *s = va_arg(args, char *);
+					while (*s)
+					{
+						putchar(*s);
+						s++;
+						count++;
+					}
+					break;
+				}
+				case '%':
+				{
+					putchar('%');
+					count++;
+					break;
+				}
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+
+	va_end(args);
+	return count;
